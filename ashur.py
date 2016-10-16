85389ba41e815300c1fb4fcf2187513b12f0d153
Stokori <stokori@gmail.com>
2016-10-15 19:38:27 -0500

    trying to make it actually connect without giving weird errors

--- ashur.py
+++ ashur.py
@@ -14,78 +14,68 @@ logger.addHandler(handler)
 # Config file
 settings = configparser.ConfigParser()
 settings.read('config.ini')
+token = settings.get('Connection', 'Token')
 
 class AshurBot(discord.Client):
 
-    client = discord.Client()
-
     def __init__(self):
-        # Initialize Client
-        self.token = settings.get('Connection', 'Token')
         self.ownerID = settings.get('Owner', 'ID')
         self.ownerName = settings.get('Owner', 'Name')
         self.prefix = settings.get('Options', 'Prefix')
 
+    #various commands below that need docstrings...
     async def c_ping(self, message, params):
-        await client.send_message(message.channel, "Pong!")
+        await self.send_message(message.channel, "Pong!")
         
     async def c_hello(self, message, params):
-        await client.send_message(message.channel, "Hello, " + message.author.name + ".")
+        await self.send_message(message.channel, "Hello, " + message.author.name + ".")
         
     async def c_help(self, message, params):
-        message = "Commands:\n```"
+        
+        text = "Commands:\n```"
 
         for func in dir(self):
             if func.startswith('c_') and func != 'c_help':
-                messages += func.replace("c_", prefix) + "\n"
+                text += func.replace("c_", self.prefix) + "\n"
         
-        message += "```"
+        text += "```"
         
-        await client.send_message(message.channel, message)
+        await self.send_message(message.channel, text)
         
     async def c_restart(self, message, params):
-        if (message.author.id == ownerID):
-            await client.send_message(message.channel, "Restarting...")
+        if (message.author.id == self.ownerID):
+            await self.send_message(message.channel, "Restarting...")
             os.execl(sys.executable, sys.executable, *sys.argv)
         else:
-            await client.send_message(message.channel, "Sorry, only my owner(" + ownerName + ") can do that. Please @ them if I need to be restarted.")
+            await self.send_message(message.channel, "Sorry, only my owner(" + ownerName + ") can do that. Please @ them if I need to be restarted.")
         
     async def c_exit(self, message, params):
-        if (message.author.id == ownerID):
-            await client.send_message(message.channel, "Later, nerds.")
+        if (message.author.id == self.ownerID):
+            await self.send_message(message.channel, "Later, nerds.")
             quit()
         else:
-            await client.send_message(message.channel, "Sorry, only my owner(" + ownerName + ") can do that. Please @ them if I need to be exited.")
+            await self.send_message(message.channel, "Sorry, only my owner(" + ownerName + ") can do that. Please @ them if I need to be exited.")
     
-    @client.event
-    async def on_ready():
+    async def on_ready(self):
         print('Logged in successfully.')
-        #I have no idea how to make it print this info from here, because I can't pass in self.
-        #It's not particularly important though.
-        #print(self.client.user.name)
-        #print(self.client.user.id)
+        print(self.user.name)
+        print(self.user.id)
         print('------')
     
-    @client.event
     async def on_message(self, message):
-        
         params = message.content.split(" ", 1)
         
-        if message.content.startswith(prefix + 'ping'):
-            await c_ping(message, params)
-        if message.content.startswith(prefix + 'hello'):
-            await c_hello(message, params)
-        if message.content.startswith(prefix + 'help'):
-            await c_help(message, params)
-        if message.content.startswith(prefix + 'exit'):
-            await c_exit(message, params)
-        if message.content.startswith(prefix + 'restart'):
-            await c_restart(message, params)
-
-    def start(self):
-        self.client.run(self.token)
-        print('Bot started.')
+        if message.content.startswith(self.prefix + 'ping'):
+            await self.c_ping(message, params)
+        if message.content.startswith(self.prefix + 'hello'):
+            await self.c_hello(message, params)
+        if message.content.startswith(self.prefix + 'help'):
+            await self.c_help(message, params)
+        if message.content.startswith(self.prefix + 'exit'):
+            await self.c_exit(message, params)
+        if message.content.startswith(self.prefix + 'restart'):
+            await self.c_restart(message, params)
 
 if __name__ == "__main__":
     ashur = AshurBot()
-    ashur.start()
+    ashur.run(token)
